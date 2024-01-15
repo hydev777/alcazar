eaf42469e28dc5b880dd002f49c372916093e1d0
hydev777 <hytechwilson@gmail.com>
2024-01-15 14:22:35 -0400

    change(app): change project name

--- client/alcazar/android/app/src/main/kotlin/com/example/alcazar_front_end/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.alcazar_front_end
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- client/alcazar/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'app/view/app.dart';
-
-void main() {
-  runApp(
-    const App(),
-  );
-}
